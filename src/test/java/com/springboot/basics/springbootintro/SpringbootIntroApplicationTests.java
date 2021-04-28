6faa8b7f78e7e77be9187f766fb981b02d22769e
marco-amorim <marcoamorim95@hotmail.com>
2021-04-28 17:22:33 -0300

    project setup

---
+++ src/test/java/com/springboot/basics/springbootintro/SpringbootIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.basics.springbootintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

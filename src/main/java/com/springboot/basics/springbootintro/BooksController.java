ac9d0a85bdb8778462ee53bdb35610aa11dee145
marco-amorim <marcoamorim95@hotmail.com>
2021-04-29 13:25:16 -0300

    refactor resp api to send status code with response entity

--- src/main/java/com/springboot/basics/springbootintro/BooksController.java
+++ src/main/java/com/springboot/basics/springbootintro/BooksController.java
@@ -1,16 +1,23 @@
 package com.springboot.basics.springbootintro;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class BooksController {
 
     @GetMapping("/books")
-    public List<Book> getAllBooks() {
-        return Arrays.asList(new Book(1, "Book 1", "Author 1"), new Book(2, "Book 2", "Author 2"));
+    public ResponseEntity<List<Book>> getAllBooks() {
+        List<Book> books = new ArrayList<Book>();
+
+        books.add(new Book(1, "Book 1", "Author 1"));
+        books.add(new Book(2, "Book 2", "Author 2"));
+
+        return ResponseEntity.status(HttpURLConnection.HTTP_OK).body(books);
     }
 }

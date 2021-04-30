11825cd03c96f58d347e155823ecce35cb2a71e7
marco-amorim <marcoamorim95@hotmail.com>
2021-04-30 17:17:18 -0300

    testing post api to save books

--- src/main/java/com/springboot/basics/springbootintro/BooksController.java
+++ src/main/java/com/springboot/basics/springbootintro/BooksController.java
@@ -2,6 +2,8 @@ package com.springboot.basics.springbootintro;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.HttpURLConnection;
@@ -19,5 +21,15 @@ public class BooksController {
         books.add(new Book(2, "Book 2", "Author 2"));
 
         return ResponseEntity.status(HttpURLConnection.HTTP_OK).body(books);
+    }
+
+    @PostMapping("/books")
+    public ResponseEntity<Book> saveBook(@RequestBody Book requestBook) {
+
+        System.out.println("Book saved!");
+        System.out.println(requestBook.toString());
+
+        return ResponseEntity.status(HttpURLConnection.HTTP_OK).body(requestBook);
+
     }
 }
